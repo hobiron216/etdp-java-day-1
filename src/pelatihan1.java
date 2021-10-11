d7028e4447b13776536801d45732988a236f55a0
Muhammad Shahid Setiawan <shahid@jkt1.ebdesk.com>
2021-10-11 15:15:25 +0700

    initial commit

---
+++ src/pelatihan1.java
@@ -0,0 +1,79 @@
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class pelatihan1 {
+    public static void main(String[] args) {
+        ArrayList<String> Karyawan = new ArrayList<>();
+        Karyawan.add("shahid setiawan");
+        Karyawan.add("fanan");
+
+        String mode, newKaryawan, namaKaryawan, ubahKaryawan, hapusKaryawan;
+        boolean b = false;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        while (!b){
+            System.out.println("### Pendataan Karyawan PT. Ebdesk Teknologi ###");
+            System.out.print("Mode: ");
+            mode = keyboard.nextLine().toLowerCase(Locale.ROOT);
+
+
+            switch (mode) {
+                case "baca":
+                    System.out.println(Karyawan);
+                    break;
+                case "simpan":
+                    System.out.print("Masukan Nama Karyawan Baru: ");
+                    newKaryawan = keyboard.nextLine();
+                    Karyawan.add(newKaryawan);
+                    System.out.println(Karyawan);
+                    break;
+                case "ubah":
+                    System.out.print("Masukan Nama Karyawan : ");
+                    namaKaryawan = keyboard.nextLine();
+                    int index = Karyawan.indexOf(namaKaryawan);
+                    if (index<0){
+                        System.out.print("Nama Karyawan Salah!!! \n");
+                    }
+                    else {
+                        System.out.print("Masukan Nama Karyawan Yang di Ubah: ");
+                        ubahKaryawan = keyboard.nextLine();
+                        Karyawan.set(index,ubahKaryawan);
+                        System.out.println(Karyawan);
+                    }
+
+                    break;
+                case "hapus":
+                    System.out.print("Masukan Nama Karyawan Yang di Hapus: ");
+                    hapusKaryawan = keyboard.nextLine();
+                    int index2 = Karyawan.indexOf(hapusKaryawan);
+                    if (index2<0){
+                        System.out.print("Nama Karyawan Salah!!! \n");
+                    }
+                    else {
+                        Karyawan.remove(hapusKaryawan);
+                        System.out.println(Karyawan);
+                    }
+                    break;
+                case "keluar":
+                    b = true;
+                    break;
+                default:
+                    b = true;
+                    break;
+            }
+        }
+
+        System.exit(0);
+
+
+//        else if (mode==)
+//        // Menampilkan apa yang sudah simpan di variabel
+//        System.out.println("--------------------");
+//        System.out.println("Nama Karyawan: " + nama);
+//        System.out.println("Alamat: " + alamat);
+//        System.out.println("Usia: " + usia + " tahun");
+//        System.out.println("Gaji: Rp " + gaji);
+    }
+}
